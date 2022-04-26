30d1272610252db5afe853321d0371a75f35d7ef
Karina2510 <karina.ferreira.s1995@gmail.com>
2022-04-26 03:13:47 -0300

    Inclusão dos novos conteúdos realizados nos cursos

---
+++ cursos_DIO_GFT/6 - Variáveis, Tipos de Dados e Operadores Matemáticos em Java/praticandoExercicios/praticandoExercicios/src/exercicios/diversasVariaveis.java
@@ -0,0 +1,53 @@
+package exercicios;
+
+public class diversasVariaveis {
+
+    public static void main(String[] args) {
+        byte variavelByte1 = 15;
+        byte variavelByte2 = -15;
+
+        short variavelShot1 = 14256;
+        short variavelShot2 = -14256;
+
+        int variavelInt1 = -526296;
+        int variavelInt2 = 526296;
+
+        long variavelLong1 = -985691978L;
+        long variavelLong2 = 985691978L;
+
+        float variavelFloat1 = -5.1617F;
+        float variavelFloat2 = 5.1617F;
+
+        double variavelDouble1 = 17.58;
+        double variavelDouble2 = -17.58;
+
+        char variavelChar1 = 'w';
+        char variavelChar2 = '\u0057';
+
+        String variavelString1 = "java";
+        String variavelString2 = "Java, uma linguagem fortemente tipada";
+
+        boolean varLogica1 = true;
+        boolean varLogica2 = false;
+
+        System.out.println(variavelByte1);
+        System.out.println(variavelByte2);
+        System.out.println(variavelShot1);
+        System.out.println(variavelShot2);
+        System.out.println(variavelInt1);
+        System.out.println(variavelInt2);
+        System.out.println(variavelLong1);
+        System.out.println(variavelLong2);
+        System.out.println(variavelFloat1);
+        System.out.println(variavelFloat2);
+        System.out.println(variavelDouble1);
+        System.out.println(variavelDouble2);
+        System.out.println(variavelChar1);
+        System.out.println(variavelChar2);
+        System.out.println(variavelString1);
+        System.out.println(variavelString2);
+        System.out.println(varLogica1);
+        System.out.println(varLogica2);
+
+    }
+}
